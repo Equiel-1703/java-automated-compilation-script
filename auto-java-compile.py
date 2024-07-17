7bfe38c046e1b657d5a6662a2d7a837621b5b0ae
Henrique Rodrigues <henrique.grdr@inf.ufpel.edu.br>
2024-07-17 12:11:09 -0300

    refactor: finished changes and improved project organization

--- auto-java-compile.py
+++ auto-java-compile.py
@@ -6,33 +6,34 @@ from exceptions import *
 from compilation_options import CompilationOptions
 from terminal_colors import TerminalColors
 
-def show_menu(src_folder) -> str:
+def show_menu(compilation_options: CompilationOptions) -> str:
 	"""
 	Shows a menu with the available options to the user and the path of the informed source folder.
 
 	Args:
-		src_folder (str): The absolute path of the source folder.
+		compilation_options (CompilationOptions): The compilation options to use when compiling the files.
 	
 	Returns:
-		str: The user's choice.
+		str: The choice made by the user.
 	"""
 	print(f"{TerminalColors.HEADER} - Automated Java Compilation Script by Equiel_1703 - {TerminalColors.ENDC}")
 	print()
-	print(f"{TerminalColors.OKBLUE}Source Folder (absolute path): {src_folder}{TerminalColors.ENDC}")
+	print(f"{TerminalColors.OKBLUE}+ Source Folder (absolute path): {compilation_options.src_folder}")
+	print(f"+ Class Path (absolute path): {compilation_options.class_path}")
+	print(f"+ Main Class Path: {compilation_options.main_class_path}{TerminalColors.ENDC}")
 	print()
 	print("Choose an option:")
 	print("1. Compile all Java files in the source folder")
 	print("2. Compile specific Java files in the source folder")
 	print("3. Clear build")
-	print("4. Compile and run a specific Java file")
-	print("5. Compile and run the entire program")
-	print("6. Setup compilation options")
+	print("4. Compile and run project")
+	print("5. Setup compilation options")
 	print("0. Exit")
 
 	choice = input().strip()
 	return choice
 
-def posorder_traversal(folder, condition) -> list[str]:
+def _posorder_traversal(folder, condition) -> list[str]:
 	"""
 	Recursively traverses a folder and its subfolders in postorder,
 	returning a list of absolute paths of files that satisfy the condition function.
@@ -54,7 +55,7 @@ def posorder_traversal(folder, condition) -> list[str]:
 
 		if os.path.isdir(file):
 			# If the file is a folder, recursively traverse it and add the files to the list
-			list_of_files.extend(posorder_traversal(file, condition))
+			list_of_files.extend(_posorder_traversal(file, condition))
 		elif condition(file):
 			# If "file" is a file, we must add it to the list - only if it satisfies the condition
 			list_of_files.append(file)
@@ -62,7 +63,7 @@ def posorder_traversal(folder, condition) -> list[str]:
 	# Return the list of files
 	return list_of_files
 
-def compile_file(path_to_file: str, compilation_options: CompilationOptions) -> None:
+def _compile_file(path_to_file: str, compilation_options: CompilationOptions) -> None:
 	"""
 	Compiles a Java file.
 
@@ -89,6 +90,53 @@ def compile_file(path_to_file: str, compilation_options: CompilationOptions) -> 
 
 		raise CompilationError(f"Could not compile the file \"{path_to_file}\".")
 
+def _process_args() -> CompilationOptions:
+	"""
+	Processes the arguments provided by the user and returns the compilation options based on them.
+
+	Returns:
+		CompilationOptions: The compilation options based on the arguments provided by the user
+	"""
+
+	# Check if the user provided too many arguments
+	if len(sys.argv) > 4:
+		print(f"{TerminalColors.FAIL}Error> Too many arguments provided!{TerminalColors.ENDC}", file=sys.stderr)
+		print(f"Usage: python {sys.argv[0]} [source_folder] [class_path]", file=sys.stderr)
+		exit(1)
+
+	if len(sys.argv) == 1:
+		# If no arguments were provided, we must ask the user for the source folder and the main class path
+		src_folder = input("Folder containing the source code: ").strip()
+		main_path = input("Main class path: ").strip()
+	elif len(sys.argv) == 2:
+		# If the user provided a single argument, we must use it as the source folder and ask for the main class path
+		src_folder = sys.argv[1]
+		main_path = input("Main class path: ").strip()
+	elif len(sys.argv) == 3:
+		# If the user provided two arguments, we must use them as the source folder and the main class path
+		src_folder = sys.argv[1]
+		main_path = sys.argv[2]
+	
+	if len(sys.argv) == 4:
+		# If the user provided three arguments, we must use them as the source folder, the main class path, and the class path
+		src_folder = sys.argv[1]
+		main_path = sys.argv[2]
+		class_path = sys.argv[3]
+
+		# Create compilation options
+		compilation_options = CompilationOptions(src_folder, main_path, class_path)
+	else:
+		# Check if user want to define the class path
+		class_path = input("Class path (leave empty to use the source folder as the class path): ").strip()
+		
+		if class_path == "":
+			class_path = src_folder
+		
+		# Create compilation
+		compilation_options = CompilationOptions(src_folder, main_path, class_path)
+	
+	return compilation_options
+
 def compile_all_files(compilation_options: CompilationOptions) -> None:
 	"""
 	Compiles all Java files in the source folder.
@@ -97,7 +145,7 @@ def compile_all_files(compilation_options: CompilationOptions) -> None:
 		compilation_options (CompilationOptions): The compilation options to use when compiling the files.
 	"""
 	# Get the list of files that will be compiled
-	files_to_compile = posorder_traversal(compilation_options.src_folder, lambda file: file.endswith(".java"))
+	files_to_compile = _posorder_traversal(compilation_options.src_folder, lambda file: file.endswith(".java"))
 
 	# Print the list of files
 	print("Files to compile:")
@@ -108,7 +156,7 @@ def compile_all_files(compilation_options: CompilationOptions) -> None:
 		# Now we can compile the files in the list
 		for file in files_to_compile:
 			print(f"\n{TerminalColors.OKCYAN}Compiling file: {file}{TerminalColors.ENDC}")
-			compile_file(file, compilation_options)
+			_compile_file(file, compilation_options)
 	except CompilationError:
 		print(f"\n{TerminalColors.FAIL}Error> Could not compile all files!{TerminalColors.ENDC}", file=sys.stderr)
 		return
@@ -123,7 +171,7 @@ def compile_specific_files(compilation_options: CompilationOptions) -> None:
 	Args:
 		compilation_options (CompilationOptions): The compilation options to use when compiling.
 	"""
-	files_available = posorder_traversal(compilation_options.src_folder, lambda file: file.endswith(".java"))
+	files_available = _posorder_traversal(compilation_options.src_folder, lambda file: file.endswith(".java"))
 
 	# Print the list of files to the user
 	print("Files available to compile:")
@@ -145,7 +193,7 @@ def compile_specific_files(compilation_options: CompilationOptions) -> None:
 	try:
 		for file in files_to_compile:
 			print(f"\n{TerminalColors.OKCYAN}Compiling file: {file}{TerminalColors.ENDC}")
-			compile_file(file, compilation_options)
+			_compile_file(file, compilation_options)
 	except CompilationError:
 		print(f"\n{TerminalColors.FAIL}Error> Could not compile all files!{TerminalColors.ENDC}", file=sys.stderr)
 		return
@@ -153,14 +201,14 @@ def compile_specific_files(compilation_options: CompilationOptions) -> None:
 	print(f"\n{TerminalColors.OKGREEN}Compilation finished!{TerminalColors.ENDC}")
 	print(f"Total of {len(files_to_compile)} files compiled.")
 
-def clear_build(src_folder) -> None:
+def clear_build(compilation_options) -> None:
 	"""
 	Removes all .class files from the source folder.
 
 	Args:
-		src_folder (str): The absolute path of the source folder.
+		compilation_options (CompilationOptions): The compilation options to use when removing the files.
 	"""
-	files_to_delete = posorder_traversal(src_folder, lambda file: file.endswith(".class"))
+	files_to_delete = _posorder_traversal(compilation_options.src_folder, lambda file: file.endswith(".class"))
 
 	# Print to the user
 	print(f"{TerminalColors.OKCYAN}Cleaning build...{TerminalColors.ENDC}")
@@ -171,6 +219,90 @@ def clear_build(src_folder) -> None:
 		print(f"{TerminalColors.WARNING}Deleted: {file}{TerminalColors.ENDC}")
 	
 	print(f"\n{TerminalColors.OKGREEN}Build cleaned!{TerminalColors.ENDC}")
+
+def compile_and_run_project(compilation_options: CompilationOptions):
+	"""
+	Compiles and runs the project.
+
+	Args:
+		compilation_options (CompilationOptions): The compilation options to use when compiling and running the project.
+	"""
+	# Get path of the main file
+	main_file = os.path.join(compilation_options.src_folder, compilation_options.main_class_path.replace(".", os.sep) + ".java")
+	
+	# Compile the main file
+	_compile_file(main_file, compilation_options)
+
+	# Run the project
+	command = ["java", "-cp", compilation_options.class_path, compilation_options.main_class_path]
+	result = subprocess.run(command, capture_output=True, text=True)
+
+	if result.returncode != 0:
+		print(f"{TerminalColors.FAIL}Error> Could not run the project!{TerminalColors.ENDC}", file=sys.stderr)
+		print("\n" + result.stderr, file=sys.stderr)
+		return
+
+	print("\n" + result.stdout)
+
+def setup_compilation_options(compilation_options: CompilationOptions):
+	"""
+	Setup the compilation options for the program.
+
+	Args:
+		compilation_options (CompilationOptions): The compilation options to setup.
+	"""
+	while True:
+		# Print current compilation options
+		print(f"{TerminalColors.OKBLUE}Current compilation options:\n{compilation_options}{TerminalColors.ENDC}")
+
+		# Ask the user what they want to change
+		print("\nChoose the option to change:")
+		print("1. Source folder")
+		print("2. Main class path")
+		print("3. Class path")
+		print("4. Verbose")
+		print("5. Encoding")
+		print("6. Compiler")
+		print("0. Return")
+
+		choice = input().strip()
+
+		if choice == "1":
+			# Change the source folder
+			src_folder = input("New source folder: ").strip()
+			compilation_options.set_src_folder(src_folder)
+		elif choice == "2":
+			# Change the main class path
+			main_class_path = input("New main class path: ").strip()
+			compilation_options.set_main_class_path(main_class_path)
+		elif choice == "3":
+			# Change the class path
+			class_path = input("New class path: ").strip()
+			compilation_options.set_class_path(class_path)
+		elif choice == "4":
+			# Change the verbose option
+			verbose = input("Verbose (y/n): ").strip().lower()
+			if verbose == "y":
+				compilation_options.set_verbose(True)
+			else:
+				compilation_options.set_verbose(False)
+		elif choice == "5":
+			# Change the encoding
+			encoding = input("New encoding: ").strip()
+			compilation_options.set_encoding(encoding)
+		elif choice == "6":
+			# Change the compiler
+			compiler = input("New compiler: ").strip()
+			compilation_options.set_compiler(compiler)
+		elif choice == "0":
+			break
+		else:
+			print(f"{TerminalColors.FAIL}Error> Invalid choice! Try again.{TerminalColors.ENDC}", file=sys.stderr)
+			os.system(clear_command)
+			continue
+	
+	compilation_options.create_compilation_options_file(os.curdir)
+	print(f"{TerminalColors.OKGREEN}Compilation options updated!{TerminalColors.ENDC}")
 
 # Main program
 if __name__ == "__main__":
@@ -184,43 +316,40 @@ if __name__ == "__main__":
 		# If the OS is not Windows, we must use the "clear" command
 		clear_command = "clear"
 
-	# Check if the user provided too many arguments
-	if len(sys.argv) > 3:
-		print(f"{TerminalColors.FAIL}Error> Too many arguments provided!{TerminalColors.ENDC}", file=sys.stderr)
-		print(f"Usage: python {sys.argv[0]} [source_folder] [class_path]", file=sys.stderr)
-		exit(1)
-
-	if len(sys.argv) == 1:
-		# If no arguments were provided, we must ask the user for the source folder
-		src_folder = input("Folder containing the source code: ").strip()
-	elif len(sys.argv) == 2:
-		# If the user provided a single argument, we must use it as the source folder
-		src_folder = sys.argv[1]
-
-	if len(sys.argv) == 3:
-		# If the user provided a class path (two arguments), we must use it as the class path
-		class_path = sys.argv[2]
-		compilation_options = CompilationOptions(src_folder, class_path)
+	# Check if the compilation options file exists
+	if CompilationOptions.check_if_compilation_options_file_exists(os.curdir):
+		# If the file exists, we must load the compilation options from it
+		compilation_options = CompilationOptions.load_compilation_options_file(os.curdir)
 	else:
-		# Create Compiler Options object without class path
-		compilation_options = CompilationOptions(src_folder)
+		# If the file does not exist, we must process the arguments provided by the user
+		compilation_options = _process_args()
+		# And create the compilation options file
+		compilation_options.create_compilation_options_file(os.curdir)
 
 	# Program loop
 	while True:
-		choice = show_menu(src_folder)
+		choice = show_menu(compilation_options)
 
 		if choice == "1":
 			# Compile all Java files in the source folder
 			os.system(clear_command)
-			compile_all_files(src_folder)
+			compile_all_files(compilation_options)
 		elif choice == "2":
 			# Compile specific Java files in the source folder
 			os.system(clear_command)
-			compile_specific_files(src_folder)
+			compile_specific_files(compilation_options)
 		elif choice == "3":
 			# Clear build
 			os.system(clear_command)
-			clear_build(src_folder)
+			clear_build(compilation_options)
+		elif choice == "4":
+			# Compile and run project
+			os.system(clear_command)
+			compile_and_run_project(compilation_options)
+		elif choice == "5":
+			# Setup compilation options
+			os.system(clear_command)
+			setup_compilation_options(compilation_options)
 		elif choice == "0":
 			# Exit the program
 			print(f"{TerminalColors.OKCYAN}Exiting the program...{TerminalColors.ENDC}")
--- compilation_options.py
+++ compilation_options.py
@@ -5,29 +5,49 @@ import sys
 from terminal_colors import TerminalColors
 
 class CompilationOptions:
-	_name_of_file = "compilation_options.json"
+	name_of_file = "compilation_options.json"
+
+	src_folder: str
+	main_class_path: str
+	class_path: str
+	verbose: bool
+	encoding: str
+	compiler: str
 
 	def __init__(self, *args) -> None:
 		self.verbose = False
 		self.encoding = "UTF-8"
 		self.compiler = "javac"
 
-		if len(args) == 1:
-			self.src_folder = self._validate_folder(args[0])
+		if len(args) == 2:
+			self.set_src_folder(args[0])
+			self.main_class_path = args[1]
+			
 			# If the class path is not specified, we will use the source folder as the class path
 			self.class_path = self.src_folder
+		elif len(args) == 3:
+			self.set_src_folder(args[0])
+			self.main_class_path = args[1]
+			self.set_class_path(args[2])
 		else:
-			self.src_folder = self._validate_folder(args[0])
-			self.class_path = self._validate_folder(args[1])
+			self.src_folder = ""
+			self.main_class_path = ""
+			self.class_path = ""
+	
+	def __str__(self) -> str:
+		return f"+ Source folder: {self.src_folder}\n+ Main class path: {self.main_class_path}\n+ Class path: {self.class_path}\n+ Verbose: {self.verbose}\n+ Encoding: {self.encoding}\n+ Compiler: {self.compiler}"
 
 	def _validate_folder(self, folder_path) -> str:
 		"""
 		Validates the received folder. If the folder does not exist or it is not a path, the program will exit with an error message.
 
-		This function returns the absolute path of the folder, if it valid.
+		If it's a valid folder, the absolute path of the folder will be returned.
 
 		Args:
 			folder_path (str): The path of the folder to validate.
+		
+		Returns:
+			str: The absolute path of the folder.
 		"""
 		# Get the absolute path of the folder
 		absolute_path = os.path.abspath(folder_path)
@@ -39,7 +59,62 @@ class CompilationOptions:
 		
 		return absolute_path
 	
-	def check_if_compilation_options_file_exists(self, path: str) -> bool:
+	def set_verbose(self, verbose: bool) -> None:
+		"""
+		Sets the verbose option.
+
+		Args:
+			verbose (bool): The value to set.
+		"""
+		self.verbose = verbose
+	
+	def set_encoding(self, encoding: str) -> None:
+		"""
+		Sets the encoding option.
+
+		Args:
+			encoding (str): The value to set.
+		"""
+		self.encoding = encoding
+	
+	def set_compiler(self, compiler: str) -> None:
+		"""
+		Sets the compiler option.
+
+		Args:
+			compiler (str): The value to set.
+		"""
+		self.compiler = compiler
+
+	def set_src_folder(self, src_folder: str) -> None:
+		"""
+		Sets the source folder option.
+
+		Args:
+			src_folder (str): The value to set.
+		"""
+		self.src_folder = self._validate_folder(src_folder)
+
+	def set_main_class_path(self, main_class_path: str) -> None:
+		"""
+		Sets the main class path option.
+
+		Args:
+			main_class_path (str): The value to set.
+		"""
+		self.main_class_path = main_class_path
+
+	def set_class_path(self, class_path: str) -> None:
+		"""
+		Sets the class path option.
+
+		Args:
+			class_path (str): The value to set.
+		"""
+		self.class_path = self._validate_folder(class_path)
+
+	@staticmethod
+	def check_if_compilation_options_file_exists(path: str) -> bool:
 		"""
 		Checks if the compilation options file exists in the specified path.
 
@@ -49,8 +124,37 @@ class CompilationOptions:
 		Returns:
 			bool: True if the file exists, False otherwise.
 		"""
-		return os.path.exists(os.path.join(path, self._name_of_file))
+		return os.path.exists(os.path.join(path, CompilationOptions.name_of_file))
+	
+	@staticmethod
+	def load_compilation_options_file(path: str) -> "CompilationOptions":
+		"""
+		Loads the compilation options from the path specified. The name of the file must be %s.
 
+		Args:
+			path (str): The path of the file to load.
+		""".format(CompilationOptions.name_of_file)
+
+		path = os.path.join(path, CompilationOptions.name_of_file)
+
+		# Load the dictionary from the file
+		with open(path, "r") as file:
+			compilation_options = json.load(file)
+		
+		# Create a new CompilationOptions object with the loaded options
+		compilation_options_obj = CompilationOptions()
+
+		# It is a good idea to define later a JSON Schema to validate the file
+
+		compilation_options_obj.set_src_folder(compilation_options["src_folder"])
+		compilation_options_obj.set_main_class_path(compilation_options["main_class_path"])
+		compilation_options_obj.set_class_path(compilation_options["class_path"])
+		compilation_options_obj.set_verbose(compilation_options["verbose"])
+		compilation_options_obj.set_encoding(compilation_options["encoding"])
+		compilation_options_obj.set_compiler(compilation_options["compiler"])
+
+		return compilation_options_obj
+	
 	def create_compilation_options_file(self, path: str) -> None:
 		"""
 		Creates a compilation options file with the name %s in the specified path.
@@ -58,8 +162,8 @@ class CompilationOptions:
 		The options saved are the current ones of this object.
 		
 		Args:
-			path (str): The path of the file to create.
-		""".format(self._name_of_file)
+			path (str): The path to create the file in.
+		""".format(self.name_of_file)
 
 		# Create the dictionary with the compilation options
 		compilation_options = {
@@ -67,33 +171,12 @@ class CompilationOptions:
 			"encoding": self.encoding,
 			"compiler": self.compiler,
 			"class_path": self.class_path,
+			"main_class_path": self.main_class_path,
 			"src_folder": self.src_folder
 		}
+
+		path = os.path.join(path, self.name_of_file)
 
 		# Write the dictionary to the file
 		with open(path, "w") as file:
 			json.dump(compilation_options, file, indent=4)
-	
-	def load_compilation_options_file(self, path: str) -> None:
-		"""
-		Loads the compilation options from the path specified. The name of the file must be %s.
-
-		Args:
-			path (str): The path of the file to load.
-		""".format(self._name_of_file)
-
-		path = os.path.join(path, self._name_of_file)
-
-		# Load the dictionary from the file
-		with open(path, "r") as file:
-			compilation_options = json.load(file)
-		
-		# Set the compilation options
-		self.verbose = compilation_options["verbose"]
-		self.encoding = compilation_options["encoding"]
-		self.compiler = compilation_options["compiler"]
-		self.class_path = compilation_options["class_path"]
-		self.src_folder = compilation_options["src_folder"]
-
-			
-		
